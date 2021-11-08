fedaf08a2e79790356f63af5786da39cce89b8d5
Jan A <>
2021-11-08 11:11:56 +0100

    test

--- student.h
+++ student.h
@@ -1,13 +1,12 @@
-#include datetime.h
+#include "datetime.h"
 
 
 struct student {
     char nachmane[30];
-    char vorname[30];
     unsigned int matrikelnummer;
     struct date startDatum;
     struct date endDatum;
-
+    
 
     
        
