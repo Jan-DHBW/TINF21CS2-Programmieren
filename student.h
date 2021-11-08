c2f896bbe4be9a274dfc6e6c9246d2fbccdc5b23
Jan A <>
2021-11-08 11:03:32 +0100

    df

---
+++ student.h
@@ -0,0 +1,14 @@
+#include datetime.h
+
+
+struct student {
+    char nachmane[30];
+    char vorname[30];
+    unsigned int matrikelnummer;
+    struct date startDatum;
+    struct date endDatum;
+
+
+    
+       
+};
