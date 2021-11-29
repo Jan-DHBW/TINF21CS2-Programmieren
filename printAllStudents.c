b466f66b868b0fbc0a099ad41fbbfea2b0f5ed72
Nevermindx3 <78419513+Nevermindx3@users.noreply.github.com>
2021-11-29 11:07:50 +0100

    Abstimmung erforderlich

---
+++ printAllStudents.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int printAllStudents(){
+
+    int counter = 0;
+
+    //bubblesort
+
+    while(counter <= maxarray){
+        counter++;
+        printf("Nachname: %c \n", nachname[counter]);
+        printf("Matrikelnummer: %f \n", matrikelnummer[counter]);
+        printf("Eintrittsdatum: %c \n", startDatum[counter]);
+        printf("Austrittsdatum: %c \n", endDatum[counter]);
+        printf("Geburtsdatum: %c \n", gebDatum[counter]);
+        printf("_____________________________________________________________________________")}
+
+    return 0;
+}
