3a55570cfcd72b4e4a0378348dcfdb253e659501
Nevermindx3 <78419513+Nevermindx3@users.noreply.github.com>
2021-12-06 10:45:12 +0100

    Updated Function

--- printAllStudents.c
+++ printAllStudents.c
@@ -9,7 +9,7 @@ int printAllStudents(){
         printf("Matrikelnummer: %f \n", currentStudent.matrikelnummer);
         printf("Eintrittsdatum: %d,%d,%d \n",  currentStudent.start_dat.day, currentStudent.start_dat.month, currentStudent.start_dat.year);
         printf("Austrittsdatum: %d,%d,%d \n", currentStudent.end_dat.day, currentStudent.end_dat.month, currentStudent.end_dat.year);
-        printf("Geburtsdatum: %c \n", currentStudent.geb_dat.day, currentStudent.geb_dat.month, currentStudent.geb_dat.year);
+        printf("Geburtsdatum: %d,%d,%d \n", currentStudent.geb_dat.day, currentStudent.geb_dat.month, currentStudent.geb_dat.year);
         printf("_____________________________________________________________________________")}
 
     return 0;
