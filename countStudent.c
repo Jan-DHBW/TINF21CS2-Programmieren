4bd4453d83808d740af1a39aa35858152ac762a4
Nevermindx3 <78419513+Nevermindx3@users.noreply.github.com>
2021-12-06 11:07:14 +0100

    Anzahl der Studenten

---
+++ countStudent.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int countStudent(){
+    int counter = 0;
+
+    struct student *currentStudent = firstStudent;
+    while (currentStudent != NULL)
+    {
+        counter++
+    }
+    printf("Anzahl der Studenten im Programm beträgt: %d \n", counter);
+
+    return 0;
+}
