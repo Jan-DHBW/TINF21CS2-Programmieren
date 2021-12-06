9ae3113268795e39055276da59fc833f735ed93c
Nevermindx3 <78419513+Nevermindx3@users.noreply.github.com>
2021-12-06 10:42:44 +0100

    Updated Function

--- printAllStudents.c
+++ printAllStudents.c
@@ -2,17 +2,14 @@
 
 int printAllStudents(){
 
-    int counter = 0;
+    structStudent *currentStudent = *firstStudent;
 
-    //bubblesort
-
-    while(counter <= maxarray){
-        counter++;
-        printf("Nachname: %c \n", nachname[counter]);
-        printf("Matrikelnummer: %f \n", matrikelnummer[counter]);
-        printf("Eintrittsdatum: %c \n", startDatum[counter]);
-        printf("Austrittsdatum: %c \n", endDatum[counter]);
-        printf("Geburtsdatum: %c \n", gebDatum[counter]);
+    while(currentStudent != NULL){
+        printf("Nachname: %c \n", currentStudent.nachname);
+        printf("Matrikelnummer: %f \n", currentStudent.matrikelnummer);
+        printf("Eintrittsdatum: %d,%d,%d \n",  currentStudent.start_dat.day, currentStudent.start_dat.month, currentStudent.start_dat.year);
+        printf("Austrittsdatum: %d,%d,%d \n", currentStudent.end_dat.day, currentStudent.end_dat.month, currentStudent.end_dat.year);
+        printf("Geburtsdatum: %c \n", currentStudent.geb_dat.day, currentStudent.geb_dat.month, currentStudent.geb_dat.year);
         printf("_____________________________________________________________________________")}
 
     return 0;
--- printStudent.c
+++ printStudent.c
@@ -1,27 +1,23 @@
 #include <stdio.h>
 
 int printStudent (){
-    int searchM;
-    int counter = 0;
+    structStudent *currentStudent = *firstStudent;
 
     printf("Geben Sie eine Matrikelnummer ein:")
     scanf("%f",  &searchM);
 
-    while(searchM != matrikelnummer[count]) {
-        counter++;
-    // IfStatement einfügen,
-    // if (counter > höchster Arrayeintrag)
-    // {printf("Martikelnummer nicht gefunden!\n");
-    // return 0;}
-    }
+    while(currentStudent != NULL) {
 
-    if (searchM == martikelnummer[counter]){
-        printf("Nachname: %c \n", nachname[counter]);
-        printf("Matrikelnummer: %f \n", matrikelnummer[counter]);
-        printf("Eintrittsdatum: %c \n", startDatum[counter]);
-        printf("Austrittsdatum: %c \n", endDatum[counter]);
-        printf("Geburtsdatum: %c \n", gebDatum[counter]);
-    }
 
-    return 0;
+        if (searchM == currentStudent.martikelnummer){
+            printf("Nachname: %c \n", currentStudent.nachname);
+            printf("Matrikelnummer: %f \n", currentStudent.matrikelnummer);
+            printf("Eintrittsdatum: %d,%d,%d \n",  currentStudent.start_dat.day, currentStudent.start_dat.month, currentStudent.start_dat.year);
+            printf("Austrittsdatum: %d,%d,%d \n", currentStudent.end_dat.day, currentStudent.end_dat.month, currentStudent.end_dat.year);
+            printf("Geburtsdatum: %c \n", currentStudent.geb_dat.day, currentStudent.geb_dat.month, currentStudent.geb_dat.year);
+            return 0;
+    }
+    structStudent currentStudent = currentStudent.next;
 }
+    printf("Matrikelnummer nicht gefunden! \n");
+    return 1;
